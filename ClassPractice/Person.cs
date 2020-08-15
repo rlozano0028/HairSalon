0d2b7b297bcff9bef4e8c627b0c2073b187d1bbe
Roman Lozano <hotrodoger@gmail.com>
2020-08-14 17:18:30 -0700

    Moved Person class to its own file.

---
+++ ClassPractice/Person.cs
@@ -0,0 +1,46 @@
+﻿namespace ClassPractice
+{
+    class Person
+    {
+        string Name;
+        string HairColor;
+        string HairStyle;
+        int HairLength;
+
+
+        public Person(string Name, string HairColor, string HairStyle, int HairLength)
+        {
+            this.Name = Name;
+            this.HairColor = HairColor;
+            this.HairStyle = HairStyle;
+            this.HairLength = HairLength;
+        }
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public string GetColor()
+        {
+            return HairColor;
+        }
+
+        public string GetStyle()
+        {
+            return HairStyle;
+        }
+        public int GetHairLength()
+        {
+            return HairLength;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, HairColor:{HairColor}, HairStyle{HairStyle}, HairLength{HairLength}";
+        }
+
+
+    }
+}
+
+
--- ClassPractice/Program.cs
+++ ClassPractice/Program.cs
@@ -6,52 +6,7 @@ namespace ClassPractice
 {
     class Program
     {
-        class Person
-        {
-            string Name;
-            string HairColor;
-            string HairStyle;
-            int HairLength;
-
-
-            public Person(string Name, string HairColor, string HairStyle, int HairLength)
-            {
-                this.Name = Name;
-                this.HairColor = HairColor;
-                this.HairStyle = HairStyle;
-                this.HairLength = HairLength;
-            }
-            public string GetName()
-            {
-                return Name;
-            }
-
-            public string GetColor()
-            {
-                return HairColor;
-            }
-
-            public string GetStyle()
-            {
-                return HairStyle;
-            }
-            public int GetHairLength()
-            {
-                return HairLength;
-            }
-
-            public override string ToString()
-            {
-                return $"{Name}, HairColor:{HairColor}, HairStyle{HairStyle}, HairLength{HairLength}";
-            }
-
-
-        }
-
-
-
-
-
+        
         static void Main(string[] args)
         {
 
@@ -106,7 +61,7 @@ namespace ClassPractice
             //Keep repeating until the user doesn't want to change anyone and display all the information when you exit
 
             bool removePerson = false;
-            bool changing = true; 
+            bool changing = true;
             string change = "";
             string ColorChange = "";
             int LengthChange = 0;
@@ -119,48 +74,48 @@ namespace ClassPractice
             People[3] = new Person("Hannah", " Brown ", " strait ", 5);
             People[4] = new Person("Juan", " Brown ", " flat ", 3);
 
-            
-                for (int a = 0; a < 5 ; a++)
-                {
+
+            for (int a = 0; a < 5; a++)
+            {
 
 
-                    
-                
+
+
                 for (int l = 0; l < People.Length; l++)
                 {
-                    Console.WriteLine($"{l+1}.) {People[l]}");
+                    Console.WriteLine($"{l + 1}.) {People[l]}");
                 }
                 Console.WriteLine();
-                
+
                 ask = 0;
                 changing = true;
                 Console.WriteLine("Who would you like to work with?");
                 int user = int.Parse(Console.ReadLine()) - 1;
-                while(user < 0 || user > People.Length)
+                while (user < 0 || user > People.Length)
                 {
                     Console.WriteLine("please enter a valid user");
                     user = int.Parse(Console.ReadLine());
-                }    
+                }
                 while (changing == true)
                 {
-                    
-                            if (ask == 0)
-                            {
-                                Console.WriteLine($"what would you like to change about {People[user].GetName()} ");
-                                
-                                Console.WriteLine("");
-                                Console.WriteLine(People[user].ToString());
-                                change = Console.ReadLine();
-                            }
-                            else if (ask >= 1)
-                            {
-                                Console.WriteLine($"Would you like to move on to the next customer or continue with {People[user].GetName()}");
-                                string answer = Console.ReadLine();
-                                if (answer == "next customer")
-                                {
-                                    changing = false;
-                                    answer = "";
-                                    removePerson = true;
+
+                    if (ask == 0)
+                    {
+                        Console.WriteLine($"what would you like to change about {People[user].GetName()} ");
+
+                        Console.WriteLine("");
+                        Console.WriteLine(People[user].ToString());
+                        change = Console.ReadLine();
+                    }
+                    else if (ask >= 1)
+                    {
+                        Console.WriteLine($"Would you like to move on to the next customer or continue with {People[user].GetName()}");
+                        string answer = Console.ReadLine();
+                        if (answer == "next customer")
+                        {
+                            changing = false;
+                            answer = "";
+                            removePerson = true;
                             if (removePerson == true)
                             {
                                 Person[] RemovePeople = new Person[People.Length - 1];
@@ -178,77 +133,72 @@ namespace ClassPractice
                                 People = RemovePeople;
                                 removePerson = false;
                             }
-                            
-                    
-                                    break;
 
 
-                                }
-                                else if (answer == "continue")
-                                {
-                                    answer = "";
-                                    ask = 0;
-                                }
-                            }
+                            break;
 
 
-
-                            if (change == "Hair Color")
-                            {
-                                Console.WriteLine("what color would you like to change it to?");
-                                ColorChange = Console.ReadLine();
-                                People[user] = new Person(People[user].GetName(), ColorChange, People[user].GetStyle(), People[user].GetHairLength());
-                                Console.WriteLine($"{People[user].GetName()} now has {ColorChange} hair.");
-                                Console.WriteLine();
-                                ask++;
-                                change = "";
-
-
-
-                            }
-                            else if (change == "Hair Length")
-                            {
-                                Console.WriteLine("How long do you want their hair to be?");
-                                LengthChange = int.Parse(Console.ReadLine());
-                                People[user] = new Person(People[user].GetName(), People[user].GetColor(), People[user].GetStyle(), LengthChange);
-                                Console.WriteLine($"{People[user].GetName()}'s Hair has a Length of {LengthChange}");
-                                Console.WriteLine();
-                                ask++;
-                                change = "";
-                            }
-                            else if (change == "Hair Style")
-                            {
-                                Console.WriteLine("What stlye would you like them to have?");
-                                StyleChange = Console.ReadLine();
-                                People[user] = new Person(People[user].GetName(), People[user].GetColor(), StyleChange, People[user].GetHairLength());
-                                Console.WriteLine($"{People[user].GetName()} now has a {StyleChange} Style of hair");
-                                Console.WriteLine();
-                                ask++;
-                                change = "";
-                            }
-                            else
-                            {
-                                Console.WriteLine();
-                                Console.WriteLine("You can change the Hair Length, Hair Color, or HairLength change one of them");
-                                Console.WriteLine();
-                                change = "";
-                                
-                            }
-                            Console.WriteLine($"This is what {People[user].GetName()} looks like now");
-                            Console.WriteLine(People[user].ToString());
-
-                        
-
-
-                       
+                        }
+                        else if (answer == "continue")
+                        {
+                            answer = "";
+                            ask = 0;
+                        }
                     }
 
+
+
+                    if (change == "Hair Color")
+                    {
+                        Console.WriteLine("what color would you like to change it to?");
+                        ColorChange = Console.ReadLine();
+                        People[user] = new Person(People[user].GetName(), ColorChange, People[user].GetStyle(), People[user].GetHairLength());
+                        Console.WriteLine($"{People[user].GetName()} now has {ColorChange} hair.");
+                        Console.WriteLine();
+                        ask++;
+                        change = "";
+
+
+
+                    }
+                    else if (change == "Hair Length")
+                    {
+                        Console.WriteLine("How long do you want their hair to be?");
+                        LengthChange = int.Parse(Console.ReadLine());
+                        People[user] = new Person(People[user].GetName(), People[user].GetColor(), People[user].GetStyle(), LengthChange);
+                        Console.WriteLine($"{People[user].GetName()}'s Hair has a Length of {LengthChange}");
+                        Console.WriteLine();
+                        ask++;
+                        change = "";
+                    }
+                    else if (change == "Hair Style")
+                    {
+                        Console.WriteLine("What stlye would you like them to have?");
+                        StyleChange = Console.ReadLine();
+                        People[user] = new Person(People[user].GetName(), People[user].GetColor(), StyleChange, People[user].GetHairLength());
+                        Console.WriteLine($"{People[user].GetName()} now has a {StyleChange} Style of hair");
+                        Console.WriteLine();
+                        ask++;
+                        change = "";
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("You can change the Hair Length, Hair Color, or HairLength change one of them");
+                        Console.WriteLine();
+                        change = "";
+
+                    }
+                    Console.WriteLine($"This is what {People[user].GetName()} looks like now");
+                    Console.WriteLine(People[user].ToString());
                 }
+
+            }
             Console.WriteLine("You have no more customers.");
             Console.WriteLine("You can now go home.");
 
-            }
         }
     }
+}
 
 
